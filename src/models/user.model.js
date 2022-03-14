54dedcc6e0def2cbbe19e4836642031f886d063a
Ayush Batra <ayushbatra128@gmail.com>
2022-03-14 14:30:44 +0530

    userfixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
-    middleName: {type: String, required: true},
+    middleName: {type: String, required: false},
     lastName: {type: String, required: true},
     age: {type: Number, required: true},
     email: {type: String, required: true, unique: true},
     address: {type: String, required: true},
     gender: {type: String, required: true},
-    type : {type: String, required: true},
+    type : {type: String, required: false},
 },{
     timestamps: true,
     versionKey: false
