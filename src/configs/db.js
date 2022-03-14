dfcfb783c1400eced55b5e50c48277b5f902cd2d
Ayush Batra <ayushbatra128@gmail.com>
2022-03-14 14:40:34 +0530

    gitguirdian fix

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
+let username = "sam7789";
+let password = "XN0ynbM9n8KmXqZL"
 const connect = ()=>{
-    return mongoose.connect("mongodb+srv://sam7789:XN0ynbM9n8KmXqZL@cluster0.omfqq.mongodb.net/evaluation?retryWrites=true&w=majority");
+    return mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.omfqq.mongodb.net/evaluation?retryWrites=true&w=majority`);
 };
 module.exports = connect;
