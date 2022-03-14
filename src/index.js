cd05a8ddb3379c5a65a93e8bccc92b92aef7aebc
Ayush Batra <ayushbatra128@gmail.com>
2022-03-14 14:27:08 +0530

    some controller added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const userController = require("./contollers/user.controller");
 
 const brachController = require("./contollers/branch.controller");
@@ -10,6 +12,10 @@ const masterController = require("./contollers/master.controller");
 const savingController = require("./contollers/saving.controller");
 
 const fixedController = require("./contollers/fixed.controller");
+
+app.use("/users",userController);
+
+app.use("/branches",brachController);
 
 module.exports  = app;
 
